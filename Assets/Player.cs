7fc9da94185a29f49f4d48bf3afd383ae16be7ae
alejandrittaa <alejandracortesreyero@gmail.com>
2024-12-04 17:54:57 +0100

    movimiento finallizado y metiendo cursor

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
     private Camera cam;
+    private NavMeshAgent agent;
     void Start()
     {
         //indicamos que camara es la que tiene que tener en cuenta
         cam = Camera.main;
+        GetComponent<NavMeshAgent>();
     }
 
     
@@ -19,6 +22,14 @@ public class Player : MonoBehaviour
         {
             //Creo un rayo desde la cámara que indica la posición del ratón
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            //tiramos/lanzamos el rayo anterior
+            if(Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                //le decimos al agent/player que tiene como destino el punto de impacto
+                agent.SetDestination(hitInfo.point);
+            }
+
         }
         
     }
