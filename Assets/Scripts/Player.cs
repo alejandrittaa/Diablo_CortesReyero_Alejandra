95af5bf9a5e31f2a9f56d5ee300d4bd4a7e475e1
alejandrittaa <alejandracortesreyero@gmail.com>
2024-12-05 18:21:26 +0100

    mas cosas nuevas para interactuar con el npc

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,6 +8,8 @@ public class Player : MonoBehaviour
 {
     private Camera cam;
     private NavMeshAgent agent;
+    //almaceno el último tranform que clicke con el raton
+    private Transform ultimoClick;
     void Start()
     {
         //indicamos que camara es la que tiene que tener en cuenta
@@ -14,8 +17,13 @@ public class Player : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
     }
 
-    
     void Update()
+    {
+        Movimiento();
+        ComprobarInteraccion();
+    }
+
+    private void Movimiento()
     {
         //si el player hace click izquierdo, el personaje se moverá hacía la posición que indique el ratón
         if (Input.GetMouseButtonDown(0))
@@ -24,13 +32,31 @@ public class Player : MonoBehaviour
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
             //tiramos/lanzamos el rayo anterior
-            if(Physics.Raycast(ray, out RaycastHit hitInfo))
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
             {
                 //le decimos al agent/player que tiene como destino el punto de impacto
                 agent.SetDestination(hitInfo.point);
-            }
 
+                //actualizo el último hit con el transform que acabo de clickar
+                ultimoClick = hitInfo.transform;
+            }
         }
-        
+    }
+
+    private void ComprobarInteraccion()
+    {
+        //si el ultimo click que hice fue en el npc...
+        if(ultimoClick != null && ultimoClick.TryGetComponent(out NPC npc))
+        {
+            //actualizamos la distancia de parada para no superponernos al npc
+            agent.stoppingDistance = 2f; 
+
+            //comprobar si ya hemos llegado a dicho destino (delante del npc)
+            if(agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+            {
+                //si hemos llegado, llamamos al metodo interacción de dentro del npc
+                npc.Interactuar();
+            }
+        }
     }
 }
