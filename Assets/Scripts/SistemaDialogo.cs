ecf983aea7b75235662eba4278f66f79e2db3f19
alejandrittaa <alejandracortesreyero@gmail.com>
2024-12-11 17:31:58 +0100

    cambios en clase

--- Assets/Scripts/SistemaDialogo.cs
+++ Assets/Scripts/SistemaDialogo.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SistemaDialogo : MonoBehaviour
 {
+
+    [SerializeField] private GameObject marcoDialogo; //marco a habilitar/deshabilitar
+    [SerializeField] private TMP_Text textoDialogo; //dialogos como tal
+
+    private bool escribiendo;
+    private int indiceFraseActual = 0; //para por cual frase va
+
+    private DialogoSO dialogoActual; //para saber cual es el dialogo con el que estamos trabajando en cada momento.
+
+
     //PATRÓN SINGLETON:
 
         //1. Solo existe una instancia de SistemaDialogo.
@@ -12,7 +23,7 @@ public class SistemaDialogo : MonoBehaviour
         //2. Es accesible desde cualquier punto del programa.
 
     public static SistemaDialogo sistema;
-
+    
     //AWAKE:
         //Se ejecuta ANTES del Start, independientemente de que el gameObject este activo o no.
             //Es decir, el patrón singleton se ejecuta en el Awake, porque "para empezar una clase, tiene que estar el profesor"          
@@ -33,12 +44,31 @@ public class SistemaDialogo : MonoBehaviour
 
     public void InciarDialogo(DialogoSO dialogo)
     {
-        
+        //el diálogo actual que tenemos que tratar, es el que nos pasan por parámetro
+        dialogoActual = dialogo;
+        marcoDialogo.SetActive(true);
     }
 
-
-    void Update()
+    //sirve para escribir la frase letra por letra
+    private void EscribirFrase()
     {
-        
+
     }
+
+    //sirve para autocompletar la frase
+    private void CompletarFrase()
+    {
+
+    }
+
+    private void SiguienteFrase()
+    {
+
+    }
+
+    private void FinalizarDialogo()
+    {
+
+    }
+
 }
