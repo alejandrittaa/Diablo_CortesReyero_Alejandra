ffa3d4df481cdce39f32da13d63aea65519946b8
DESKTOP-GV16N45\Alumno <alejandracortesreyero@gmail.com>
2025-01-23 19:20:12 +0100

    event manager para sacar mision de setas al hablar con el npc (SIN TERMINAR)

---
+++ Assets/Scripts/EventManagerSO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EventManager")]
+public class EventManagerSO : ScriptableObject
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
