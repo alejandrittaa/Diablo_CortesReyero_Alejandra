8679bcbfcc1ce0f5c021f2806c118fe0315f1c61
DESKTOP-GV16N45\Alumno <alejandracortesreyero@gmail.com>
2025-01-29 17:28:46 +0100

    popeo de mision funcional

--- Assets/Scripts/SistemaMisiones.cs
+++ Assets/Scripts/SistemaMisiones.cs
@@ -15,7 +15,16 @@ public class SistemaMisiones : MonoBehaviour
 
     private void EncenderToggleMision(MisionSO mision)
     {
-        
+        togglesMision[mision.indiceMision].TextoMision.text = mision.ordenInicial; //cambiamos el texto al que corresponde de la mision
+
+        //comprueba a ver si tiene repetición...
+        if(mision.tieneRepeticion)
+        {
+            //mostramos por pantalla la cantidad de repeteciones que llevas
+            togglesMision[mision.indiceMision].TextoMision.text += "(" + mision.repeticionActual + "/" + mision.totalRepeticiones + ")";
+        }
+
+        togglesMision[mision.indiceMision].gameObject.SetActive(true); //enciendo el toggle para que se vea en pantalla
     }
 
     void Start()
